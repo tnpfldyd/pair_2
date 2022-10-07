6079c1b09d966ebf14b3d39f9e9bebfb709433c6
tnpfldyd <dkdldhs0322@gmail.com>
2022-10-07 16:11:12 +0900

    1007

--- reviews/models.py
+++ reviews/models.py
@@ -10,7 +10,7 @@ class Movie(models.Model):
 
 
 class Review(models.Model):
-    movie_id = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     title = models.CharField(max_length=30)
     content = models.TextField()
     movie_name = models.CharField(max_length=30)
